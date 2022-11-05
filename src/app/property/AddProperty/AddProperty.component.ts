9d309c1cc6eb2785a7722af081b8460878d6daff
santo-19 <santo.brighton2001@gmail.com>
2022-11-05 17:29:06 +0530

    first commit

--- src/app/property/AddProperty/AddProperty.component.ts
+++ src/app/property/AddProperty/AddProperty.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TabsetComponent } from 'ngx-bootstrap/tabs';
 import { Ipropertybase } from 'src/app/models/ipropertybase';
@@ -10,10 +10,14 @@ import { Ipropertybase } from 'src/app/models/ipropertybase';
   styleUrls: ['./AddProperty.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-  @ViewChild('Form') addPropertyForm: NgForm;
+ // @ViewChild('Form') addPropertyForm: NgForm;
   @ViewChild('formTabs') formTabs: TabsetComponent;
+
+  addPropertyForm: FormGroup
+
   propertyTypes: Array<string> = ['House','Apartment','Duplex']
   furnishTypes: Array<string> = ['Fully','Semi','Unfurnished']
+
 
   propertyView : Ipropertybase = {
     Id : null,
@@ -27,19 +31,35 @@ export class AddPropertyComponent implements OnInit {
     City: null,
     RTM: null
   };
-  constructor(private router: Router) { }
 
+  constructor(private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.CreateAddPropertyForm();
+  }
 
+  CreateAddPropertyForm(){
+    this.addPropertyForm = this.fb.group({
+      BasicInfo: this.fb.group({
+        SellRent: [null, Validators.required],
+        PType: [null, Validators.required],
+        Name: [null, Validators.required]
+      }),
+      PriceInfo: this.fb.group({
+        Price: [null, Validators.required],
+        BuiltArea: [null, Validators.required]
+      })
+
+    })
   }
 
   onBack(){
     this.router.navigate(['/']);
   }
 
-  onSubmit(Form : NgForm){
+  onSubmit(){
     console.log('Congrats, form submitted');
+    console.log('SellRent=' + this.addPropertyForm.value.BasicInfo.SellRent);
     console.log(this.addPropertyForm);
   }
 
